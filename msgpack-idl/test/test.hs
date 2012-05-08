018d6c022999e8694da39b254306570a3f14c8aa
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-05-08 20:52:56 +0900

    add test (pending)

---
+++ msgpack-idl/test/test.hs
@@ -0,0 +1,18 @@
+import Test.Hspec.Monadic
+
+main :: IO ()
+main = hspecX $ do
+  describe "parser" $ do
+    it "can parse xxx..." $ do
+      pending
+
+  describe "checker" $ do
+    it "can parse xxx..." $ do
+      pending
+
+  describe "generator" $ do
+    describe "haskell" $ do
+      it "can generate client" $ do
+        pending
+      it "can communicate reference server" $ do
+        pending
