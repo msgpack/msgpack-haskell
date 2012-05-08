1e43c0e48cd39ec415197a810a22a8415b4727e4
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-05-08 20:31:43 +0900

    implementing generic deriving

---
+++ msgpack/Data/MessagePack/Generic.hs
@@ -0,0 +1,37 @@
+module Data.MessagePack.Generic (
+  GPackable(..),
+  ) where
+
+import Blaze.ByteString.Builder
+import Data.Monoid
+import GHC.Generic
+
+packUnit = undefined
+
+class GPackable f where
+  gfrom :: f a -> Builder
+
+instance GPackable U1 where
+  gfrom _ = pack ()
+  {-# INLINE gfrom #-}
+
+instance (GPackable a, GPackable b) => GPackable (a :*: b) where
+  gfrom (a :*: b) = gfrom a <> gfrom b
+  {-# INLINE gfrom #-}
+
+instance (GPackable a, GPackable b) => GPackable (a :+: b) where
+  gfrom (L1 x) = gfrom x
+  gfrom (R1 x) = gfrom x
+  {-# INLINE gfrom #-}
+
+instance GPackable a => GPackable (D1 c a) where
+  gfrom (M1 x) = from x
+  {-# INLINE gfrom #-}  
+
+instance GPackable a => GPackable (C1 c a) where
+  gfrom (M1 x) = undefined
+  {-# INLINE gfrom #-}  
+
+instance Packable a => GPackable (K1 i a) where
+  gfrom (K1 x) = from x
+  {-# INLINE gfrom #-}
