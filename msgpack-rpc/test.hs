20114b81ed00f6103a56244bdda158ec3e042a69
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-08-01 16:10:56 +0900

    adding test

--- msgpack-rpc/test.hs
+++ msgpack-rpc/test.hs
@@ -18,15 +18,27 @@ main = withSocketsDo $ hspec $ do
       server `race_` client
 
 server :: IO ()
-server = serve port [("add", toMethod add)]
+server =
+  serve port
+    [ ("add", toMethod add)
+    , ("echo", toMethod echo)
+    ]
   where
     add :: Int -> Int -> Method Int
     add x y = return $ x + y
 
+    echo :: String -> Method String
+    echo s = return $ "***" ++ s ++ "***"
+
 client :: IO ()
 client = runClient "localhost" port $ do
-  ret <- add 123 456
-  liftIO $ ret `shouldBe` 123 + 456
+  r1 <- add 123 456
+  liftIO $ r1 `shouldBe` 123 + 456
+  r2 <- echo "hello"
+  liftIO $ r2 `shouldBe` "***hello***"
   where
     add :: Int -> Int -> Client Int
     add = call "add"
+
+    echo :: String -> Client String
+    echo = call "string"
