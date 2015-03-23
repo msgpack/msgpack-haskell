8ad9ede5f75577a18b8e65d10cc7d320fc9472e9
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2015-03-23 17:18:23 +0900

    add some functions

--- msgpack-aeson/src/Data/MessagePack/Aeson.hs
+++ msgpack-aeson/src/Data/MessagePack/Aeson.hs
@@ -1,50 +1,103 @@
-{-# LANGUAGE LambdaCase    #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE LambdaCase #-}
 
--- | Aeson adapter for MessagePack
+-- | Aeson bridge for MessagePack
 
 module Data.MessagePack.Aeson (
+  -- * Conversion functions
+  toAeson, fromAeson,
+
+  -- * MessagePack instance for Aeson.Value
+  -- * ToJSON, FromJSON instance for MessagePack.Object
+
+  -- * Wrapper instances
+  AsMessagePack(..),
+  AsAeson(..),
+
+  -- * Utility functions
+  packAeson, unpackAeson,
+  decodeMessagePack, encodeMessagePack,
   ) where
 
 import           Control.Applicative
-import           Data.Aeson           as Aeson
+import           Data.Aeson           as A
 import qualified Data.ByteString.Lazy as L
 import           Data.Either
 import qualified Data.HashMap.Strict  as HM
+import           Data.Maybe
 import           Data.MessagePack     as MP
+import           Data.Monoid
 import           Data.Scientific
 import qualified Data.Text.Encoding   as T
 import qualified Data.Vector          as V
 
-fromMP :: MP.Object -> Maybe Value
-fromMP = \case
+toAeson :: MP.Object -> Maybe Value
+toAeson = \case
   ObjectNil      -> Just $ Null
   ObjectBool b   -> Just $ Bool b
   ObjectInt n    -> Just $ Number $ fromIntegral n
   ObjectFloat f  -> Just $ Number $ realToFrac f
   ObjectDouble d -> Just $ Number $ realToFrac d
   ObjectRAW b    -> String <$> either (const Nothing) Just (T.decodeUtf8' b)
-  ObjectArray v  -> Array <$> V.mapM fromMP v
+  ObjectArray v  -> Array <$> V.mapM toAeson v
   ObjectMap m ->
-    Aeson.Object . HM.fromList . V.toList
-      <$> V.mapM (\(k, v) -> (,) <$> fromObject k <*> fromMP v) m
+    A.Object . HM.fromList . V.toList
+      <$> V.mapM (\(k, v) -> (,) <$> fromObject k <*> toAeson v) m
 
-toMP :: Value -> MP.Object
-toMP = \case
-  Aeson.Object o -> ObjectMap $ V.fromList $ map (\(k, v) -> (toObject k, toMP v)) $ HM.toList o
-  Array v        -> ObjectArray $ V.map toMP v
-  String t       -> ObjectRAW $ T.encodeUtf8 t
+fromAeson :: Value -> MP.Object
+fromAeson = \case
+  Null        -> ObjectNil
+  Bool b      -> ObjectBool b
   Number s ->
     case floatingOrInteger s of
-      Left f     -> ObjectDouble f
-      Right n    -> ObjectInt n
-  Bool b         -> ObjectBool b
-  Null           -> ObjectNil
+      Left f  -> ObjectDouble f
+      Right n -> ObjectInt n
+  String t    -> ObjectRAW $ T.encodeUtf8 t
+  Array v     -> ObjectArray $ V.map fromAeson v
+  A.Object o  -> ObjectMap $ V.fromList $ map (\(k, v) -> (toObject k, fromAeson v)) $ HM.toList o
 
-newtype AsAeson = AsAeson { getAsAeson :: MP.Object }
+instance MessagePack Value where
+  fromObject = toAeson
+  toObject = fromAeson
 
-unpackMP :: FromJSON a => L.ByteString -> a
-unpackMP = undefined
+instance ToJSON MP.Object where
+  -- | When fail to convert, it returns `Null`
+  toJSON = fromMaybe Null .toAeson
 
-packMP :: ToJSON a => a -> L.ByteString
-packMP = undefined
+instance FromJSON MP.Object where
+  parseJSON = return . fromAeson
+
+newtype AsMessagePack a = AsMessagePack { getAsMessagePack :: a }
+
+instance (FromJSON a, ToJSON a) => MessagePack (AsMessagePack a) where
+  fromObject o = AsMessagePack <$> (fromJSON' =<< toAeson o)
+  toObject = fromAeson . toJSON . getAsMessagePack
+
+newtype AsAeson a = AsAeson { getAsAeson :: a }
+
+instance MessagePack a => ToJSON (AsAeson a) where
+  toJSON = fromMaybe Null . toAeson . toObject . getAsAeson
+
+instance MessagePack a => FromJSON (AsAeson a) where
+  parseJSON = maybe empty (return . AsAeson) . fromObject . fromAeson
+
+-- | pack Aeson value to msgpack binary
+packAeson :: ToJSON a => a -> L.ByteString
+packAeson = pack . toJSON
+
+-- | unpack Aeson value from msgpack binary
+unpackAeson :: FromJSON a => L.ByteString -> Maybe a
+unpackAeson b = fromJSON' =<< unpack b
+
+encodeMessagePack :: MessagePack a => a -> L.ByteString
+encodeMessagePack = encode . toJSON . AsAeson
+
+decodeMessagePack :: MessagePack a => L.ByteString -> Maybe a
+decodeMessagePack b = getAsAeson <$> (fromJSON' =<< decode b)
+
+fromJSON' :: FromJSON a => Value -> Maybe a
+fromJSON' = resultToMaybe . fromJSON
+
+resultToMaybe :: Result a -> Maybe a
+resultToMaybe = \case
+  Success a -> Just a
+  _         -> Nothing
