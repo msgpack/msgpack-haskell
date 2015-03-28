489379fa4a3d83b4d4762bd5937b775b59e60f5f
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2015-03-28 17:18:18 +0900

    add document

--- msgpack/src/Data/MessagePack.hs
+++ msgpack/src/Data/MessagePack.hs
@@ -13,22 +13,30 @@
 --------------------------------------------------------------------
 
 module Data.MessagePack (
-  module X,
-
   -- * Simple interface to pack and unpack msgpack binary
   pack, unpack,
+
+  -- * Re-export modules
+  -- $reexports
+  -- module X,
+  module Data.MessagePack.Assoc,
+  module Data.MessagePack.Get,
+  module Data.MessagePack.Object,
+  module Data.MessagePack.Put,
   ) where
 
 import           Data.Binary
 import qualified Data.ByteString.Lazy    as L
 
-import           Data.MessagePack.Assoc  as X
-import           Data.MessagePack.Get    as X
-import           Data.MessagePack.Object as X
-import           Data.MessagePack.Put    as X
+import           Data.MessagePack.Assoc
+import           Data.MessagePack.Get
+import           Data.MessagePack.Object
+import           Data.MessagePack.Put
 
+-- | Pack a Haskell value to MessagePack binary.
 pack :: MessagePack a => a -> L.ByteString
 pack = encode . toObject
 
+-- | Unpack MessagePack binary to a Haskell value. If it fails, it returns Nothing.
 unpack :: MessagePack a => L.ByteString -> Maybe a
 unpack = fromObject . decode
