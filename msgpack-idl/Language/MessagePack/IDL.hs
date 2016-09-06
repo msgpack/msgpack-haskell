2cfb003c6d050a4c8dbaaa5a4a458ae2be804b50
iphydf <iphydf@users.noreply.github.com>
2016-09-06 12:54:11 +0100

    Removed stale non-working msgpack-idl code.

--- msgpack-idl/Language/MessagePack/IDL.hs
+++
@@ -1,9 +0,0 @@
-module Language.MessagePack.IDL (
-  module Language.MessagePack.IDL.Syntax,
-  module Language.MessagePack.IDL.Parser,
-  module Language.MessagePack.IDL.CodeGen.Haskell,
-  ) where
-
-import Language.MessagePack.IDL.Syntax
-import Language.MessagePack.IDL.Parser
-import Language.MessagePack.IDL.CodeGen.Haskell
--- msgpack-idl/Language/MessagePack/IDL/Check.hs
+++
@@ -1,9 +0,0 @@
-module Language.MessagePack.IDL.Check (
-  check,
-  ) where
-
-import Language.MessagePack.IDL.Syntax
-
--- TODO: Implement it!
-check :: Spec -> Bool
-check _ = True
