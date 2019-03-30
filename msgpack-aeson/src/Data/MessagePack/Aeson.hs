894f0e5e09daf4c31ed70f7fd8b4ad52285110f3
Sam Halliday <sam.halliday@gmail.com>
2019-03-30 04:59:35 -0400

    remove orphans from the Aeson integration (#76)

--- msgpack-aeson/src/Data/MessagePack/Aeson.hs
+++ msgpack-aeson/src/Data/MessagePack/Aeson.hs
@@ -4,16 +4,9 @@
 {-# LANGUAGE LambdaCase                 #-}
 
 -- | Aeson bridge for MessagePack
-
 module Data.MessagePack.Aeson (
   -- * Conversion functions
   toAeson, fromAeson,
-
-  -- * MessagePack instance for Aeson.Value
-  -- $msgpackInstance
-
-  -- * ToJSON and FromJSON instance for MessagePack.Object
-  -- $aesonInstances
 
   -- * Wrapper instances
   AsMessagePack(..),
@@ -28,7 +21,7 @@ import           Control.Applicative
 import           Control.Arrow
 import           Control.DeepSeq
 import           Data.Aeson           as A
-import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy as L (ByteString)
 import           Data.Data
 import qualified Data.HashMap.Strict  as HM
 import           Data.Maybe
@@ -37,24 +30,24 @@ import           Data.Scientific
 import qualified Data.Text.Encoding   as T
 import qualified Data.Vector          as V
 
--- | Convert MessagePack Object to Aeson Value.
--- If the value unable to convert, it returns Nothing
-toAeson :: MP.Object -> Maybe Value
+-- | Convert 'MP.Object' to JSON 'Value'
+toAeson :: MP.Object -> A.Result Value
 toAeson = \case
-  ObjectNil      -> Just Null
-  ObjectBool b   -> Just $ Bool b
-  ObjectInt n    -> Just $ Number $ fromIntegral n
-  ObjectFloat f  -> Just $ Number $ realToFrac f
-  ObjectDouble d -> Just $ Number $ realToFrac d
-  ObjectStr t    -> Just $ String t
-  ObjectBin b    -> String <$> either (const Nothing) Just (T.decodeUtf8' b)
+  ObjectNil      -> pure Null
+  ObjectBool b   -> pure . Bool $ b
+  ObjectInt n    -> pure . Number $ fromIntegral n
+  ObjectFloat f  -> pure . Number $ realToFrac f
+  ObjectDouble d -> pure . Number $ realToFrac d
+  ObjectStr t    -> pure . String $ t
+  ObjectBin b    -> String <$> either (fail . show) pure (T.decodeUtf8' b)
   ObjectArray v  -> Array <$> V.mapM toAeson v
   ObjectMap m    ->
     A.Object . HM.fromList . V.toList
-      <$> V.mapM (\(k, v) -> (,) <$> fromObject k <*> toAeson v) m
-  ObjectExt _ _  -> Nothing
+      <$> V.mapM (\(k, v) -> (,) <$> from k <*> toAeson v) m
+      where from = maybe (fail "bad object") pure . MP.fromObject
+  ObjectExt _ _  -> fail "ObjectExt is not supported"
 
--- | Convert Aeson Value to MessagePack Object
+-- | Convert JSON 'Value' to 'MP.Object'
 fromAeson :: Value -> MP.Object
 fromAeson = \case
   Null        -> ObjectNil
@@ -67,28 +60,12 @@ fromAeson = \case
   Array v     -> ObjectArray $ V.map fromAeson v
   A.Object o  -> ObjectMap $ V.fromList $ map (toObject *** fromAeson) $ HM.toList o
 
--- $msgpackInstance
--- > instance MessagePack Value
-instance MessagePack Value where
-  fromObject = toAeson
-  toObject = fromAeson
-
--- $aesonInstances
--- > instance ToJSON Object
--- > instance FromJSON Object
-instance ToJSON MP.Object where
-  -- When fail to convert, it returns `Null`
-  toJSON = fromMaybe Null .toAeson
-
-instance FromJSON MP.Object where
-  parseJSON = return . fromAeson
-
 -- | Wrapper for using Aeson values as MessagePack value.
 newtype AsMessagePack a = AsMessagePack { getAsMessagePack :: a }
   deriving (Eq, Ord, Show, Read, Functor, Data, Typeable, NFData)
 
 instance (FromJSON a, ToJSON a) => MessagePack (AsMessagePack a) where
-  fromObject o = AsMessagePack <$> (fromJSON' =<< toAeson o)
+  fromObject o = AsMessagePack <$> (\case { A.Error _ -> Nothing; A.Success x -> Just x } $ (fromJSON =<< toAeson o))
   toObject = fromAeson . toJSON . getAsMessagePack
 
 -- | Wrapper for using MessagePack values as Aeson value.
@@ -96,31 +73,23 @@ newtype AsAeson a = AsAeson { getAsAeson :: a }
   deriving (Eq, Ord, Show, Read, Functor, Data, Typeable, NFData)
 
 instance MessagePack a => ToJSON (AsAeson a) where
-  toJSON = fromMaybe Null . toAeson . toObject . getAsAeson
+  toJSON = \case { A.Error _ -> Null; A.Success x -> x } . toAeson . toObject . getAsAeson
 
 instance MessagePack a => FromJSON (AsAeson a) where
   parseJSON = maybe empty (return . AsAeson) . fromObject . fromAeson
 
--- | Pack Aeson value to MessagePack binary
+-- | Encode to MessagePack via "Data.Aeson"'s 'ToJSON' instances
 packAeson :: ToJSON a => a -> L.ByteString
-packAeson = pack . toJSON
+packAeson = pack . fromAeson . toJSON
 
--- | Unpack Aeson value from MessagePack binary
-unpackAeson :: FromJSON a => L.ByteString -> Maybe a
-unpackAeson b = fromJSON' =<< unpack b
+-- | Decode from MessagePack via "Data.Aeson"'s 'FromJSON' instances
+unpackAeson :: FromJSON a => L.ByteString -> Result a
+unpackAeson b = fromJSON =<< toAeson =<< maybe (fail "unpackAeson") pure (unpack b)
 
--- | Encode MessagePack value to JSON
+-- | Encode MessagePack value to JSON document
 encodeMessagePack :: MessagePack a => a -> L.ByteString
 encodeMessagePack = encode . toJSON . AsAeson
 
--- | Decode MessagePack value from JSON
-decodeMessagePack :: MessagePack a => L.ByteString -> Maybe a
-decodeMessagePack b = getAsAeson <$> (fromJSON' =<< decode b)
-
-fromJSON' :: FromJSON a => Value -> Maybe a
-fromJSON' = resultToMaybe . fromJSON
-
-resultToMaybe :: Result a -> Maybe a
-resultToMaybe = \case
-  Success a -> Just a
-  _         -> Nothing
+-- | Decode MessagePack value from JSON document
+decodeMessagePack :: MessagePack a => L.ByteString -> A.Result a
+decodeMessagePack b = getAsAeson <$> (fromJSON =<< either A.Error A.Success (eitherDecode b))
--- msgpack-aeson/test/test.hs
+++ msgpack-aeson/test/test.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell     #-}
 
+import           Control.Applicative
 import           Control.Monad
 import           Data.Aeson
 import           Data.Aeson.TH
@@ -49,9 +50,9 @@ test v = do
 
 roundTrip :: (Show a, Eq a, ToJSON a, FromJSON a) => a -> IO ()
 roundTrip v = do
-  let mp = pack (AsMessagePack v)
-      v' = unpack mp
-  v' @?= Just (AsMessagePack v)
+  let mp = packAeson v
+      v' = unpackAeson mp
+  v' @?= pure v
 
 main :: IO ()
 main =
